4121bd4b6c5e854b3a31fb7cee07da5dcc746366
Peter - rsn <peterrusin.555@gmail.com>
2025-06-26 16:11:29 +0200

    Second commit

---
+++ src/Main.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello, World!");
+
+        String pozdrav = "Peter";
+
+        System.out.printf(pozdrav);
+        System.out.printf(pozdrav);
+        System.out.printf(pozdrav);
+        System.out.printf(pozdrav);
+
+        int prveCislo = 5;
+        int druheCislo = 10;
+
+
+        System.out.println(prveCislo + druheCislo);
+        System.out.println(prveCislo - druheCislo);
+        System.out.println(druheCislo / prveCislo);
+        System.out.println(prveCislo * druheCislo);
+
+        String pozdrav2 = "Ahoj";
+        String meno = "Peter";
+        System.out.println(pozdrav2 + " " + meno);
+
+        //Uloha uzivatel napise svoje meno a program ho pozdravi
+        /*String pozdrav3 = "Ahoj, ";
+        Scanner mujScanner = new Scanner(System.in);
+        System.out.println("Zadaj svoje meno:");
+        String meno = mujScanner.nextLine();
+        System.out.println((pozdrav + meno);*/
+
+        //Kalkulacka
+        int prveCislo1;
+        int druheCislo2;
+
+       /* Scanner mujScanner = new Scanner(System.in);
+
+        System.out.println("Zadaj prve cislo:");
+        prveCislo = mujScanner.nextInt();
+
+        System.out.println("Zadaj druhe cislo:");
+        prveCislo = mujScanner.nextInt();
+
+        System.out.println(prveCislo + druheCislo);
+        System.out.println(prveCislo - druheCislo);
+        System.out.println(prveCislo / druheCislo);
+        System.out.println(prveCislo * druheCislo);*/
+
+        //Uzivatel zada slovo, vo vypise budu vsetky pismena velke
+        Scanner mujScanner = new Scanner(System.in);
+        System.out.println("Zadaj svoje slovo:");
+        String slovo = mujScanner.nextLine();
+
+        System.out.println(slovo.contains("el"));
+
+
+
+
+
+    }
+}
